b0610e20e7aac192d6fa2ee321dbf1a5daa0abb3
Kayla Ross <kaylakross13@gmail.com>
2020-08-25 21:40:26 -0400

    some done

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ var originalFlavors = ["Banana Nut Fudge",
 
 (1) an array 
 
+
 Your function should return a boolean TRUE if the length of the array is 31 and FALSE if the length of the array is NOT 31.
 
 To save you from having to count the items above, you can assume that length of originalFlavors is 31. You may also want to test your function by creating and passing an array of a different length, to make sure it returns FALSE if length is not 31. 
@@ -42,7 +43,7 @@ i.e. is31Flavors(originalFlavors) will return TRUE.*/
 
 function is31Flavors(/*code here*/){
 
-    /*code here*/
+    array/
 
 }
 
